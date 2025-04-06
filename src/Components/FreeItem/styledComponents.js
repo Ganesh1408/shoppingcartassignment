5360d2b9ef655419db813ff19897f69f3b11c117
Ganesh1408 <somariganesh@gmail.com>
2025-04-06 11:35:31 +0530

    tablet view

--- src/Components/FreeItem/styledComponents.js
+++ src/Components/FreeItem/styledComponents.js
@@ -20,9 +20,15 @@ export const FreeGiftContainer = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    padding: 10px;
+    padding: 20px;
     margin:0px auto;
     margin-bottom: 10px;
+    @media screen and (max-width:768px){
+        width:80%;
+        margin:0px auto;
+        padding:16px;
+        margin:20px;
+    }
 
 `
 export const FreeGiftHeading = styled.h3`
@@ -30,12 +36,21 @@ export const FreeGiftHeading = styled.h3`
     font-weight:600;
     color: black;
     padding:5px;
+    @media screen and (max-width:768px){
+        font-size:16px;
+        padding:0px;
+    }
+
 `
 export const FreeGiftDescription = styled.p`
     font-size:16px;
     font-weight:400;
     color:grey;
     padding:5px;
+    @media screen and (max-width:768px){
+        font-size:14px;
+        padding:0px;
+    }
 `
 export const FREE = styled.h5`
     font-size:14px;
@@ -45,5 +60,10 @@ export const FREE = styled.h5`
     margin-right: 10px;
     background-color: #e0f7fa;
     border-radius: 16px;
+    @media screen and (max-width:768px){
+        font-size:12px;
+        padding:5px;
+        margin-right: 5px;
+    }
 
 `
